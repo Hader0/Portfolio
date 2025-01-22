2dca45063263a6693a9667eebe5fb2a4e1aa4b9b
Hader0 <08206953+Hader0@users.noreply.github.com>
2025-01-22 23:57:37 +1000

    Created and implemented functionality to click through projects on the project page. Also created transitioning for it

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -15,13 +15,10 @@ function Projects() {
 
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [rotation, setRotation] = useState(0);
+    const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState(false);
 
     const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
-    const rotate = () => {
-        setRotation(rotation + 180);
-    }
 
      // Check if theme is saved in local storage
     useEffect(() => {
@@ -38,36 +35,54 @@ function Projects() {
         localStorage.setItem("theme", newTheme);
     };
 
+    const nextProject = () => {
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setCurrentProjectIndex((prevIndex) => (prevIndex + 1) % Object.keys(projects).length);
+          setIsTransitioning(false);
+        }, 500);
+      };
+      
+      const previousProject = () => {
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setCurrentProjectIndex((prevIndex) => (prevIndex - 1 + Object.keys(projects).length) % Object.keys(projects).length);
+          setIsTransitioning(false);
+        }, 500);
+      };
+
+    const currentProject = projects[`project${currentProjectIndex + 1}`];
+
     return (
         <div className={`main-page ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
             <Header toggleSidebar={toggleSidebar} toggleTheme={toggleTheme} isDarkMode={isDarkMode}/>
 
             <div className="projects-body">
 
-                <div className="project-info">
-                    <p className="project-title">{projects.project1.title}</p>
+            <div className={`project-info ${isTransitioning ? 'fading' : ''}`}>
+                    <p className="project-title">{currentProject.title}</p>
                     <div className="project-icons">
-                        {projects.project1.icons.map((icon, index) => {
+                        {currentProject.icons.map((icon, index) => {
                             return <img key={index} src={icon} alt="icon" className="project-icon"/>
                         })}
                     </div>
-                    <p className="created-by">Developed by <b>{projects.project1.createdBy}</b></p>
-                    <p className="project-about">{projects.project1.about}</p>
-                    <div className="project-deployment">Site: <a href={projects.project1.deployment}>{projects.project1.deployment}</a></div>
-                    <div className="project-repository">Code: <a href={projects.project1.repository}>{projects.project1.repository}</a></div>
+                    <p className="created-by">Developed by <b>{currentProject.createdBy}</b></p>
+                    <p className="project-about">{currentProject.about}</p>
+                    <div className="project-deployment">Site: <a href={currentProject.deployment}>{currentProject.deployment}</a></div>
+                    <div className="project-repository">Code: <a href={currentProject.repository}>{currentProject.repository}</a></div>
                 </div>
 
                 <div className="seperator">
                     <hr className="rounded-projects"/>
                 </div>
 
-                <div className="project-image">
-                    <img src={`https://placehold.co/400?text=${projects.project1.title}`} alt={projects.project1.title} />
+                <div className={`project-image ${isTransitioning ? 'fading' : ''}`}>
+                    <img src={`https://placehold.co/400?text=${currentProject.title}`} alt={currentProject.title} />
                 </div>
 
-                <div className={`next-arrow arrow-theme rotate-${rotation % 360}`} onClick={rotate}>
-                    <img src={isDarkMode ? LightArrow : DarkArrow} className="arrow-up" alt="Next Section" />
-                    <img src={isDarkMode ? LightArrow : DarkArrow} className="arrow-down" alt="Next Section" />
+                <div className={`next-arrow arrow-buttons arrow-theme`}>
+                    <img src={isDarkMode ? LightArrow : DarkArrow} className="arrow-up" alt="Next Section" onClick={nextProject}/>
+                    <img src={isDarkMode ? LightArrow : DarkArrow} className="arrow-down rotate-180" alt="Next Section" onClick={previousProject}/>
                 </div>
 
             </div>
